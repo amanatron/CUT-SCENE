6028b0aa287bfec87796155754c686c9c51f389f
c-iordache <mythic1@gmail.com>
2020-07-03 20:11:33 +0000

    Added ribbon buttons and combobox

--- GUI.py
+++ GUI.py
@@ -2,6 +2,20 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from logic import form_list, STATE 
 
+#ComboBox (MAIN) -- Create a combobox that updates it's contents based on state. 
+    
+class ComboB():
+    box = Combobox()
+    def __init__(self,par):
+        self.box = Combobox(par,values=form_list("LEVEL"), postcommand = self.update_list())
+        self.box.pack(padx=(300,0),side=LEFT)
+        self.box.bind("<<ComboboxSelected>>", self.comboCallback)
+    def update_list(self):
+        print("Updqte",self.box.get())
+        self.box['values'] = form_list(self.box.get())
+    def comboCallback(self,event):
+        print("Combo",self.box.get()) 
+        self.box['values'] = form_list(self.box.get())
 
 
   
@@ -9,22 +23,42 @@ root = Tk()
 root.title("CUT-SCENE")
 root.geometry("1080x720")
 
+def boldCallback():
+    print("Bold")
+    
+def italicCallback():
+    print("Italic")
 
+def underlineCallback():
+    print("Underline")
+    
+def leftCallback():
+    print("Left")
+    
+def rightCallback():
+    print("Right")
+    
 # Ribbon Frame 
 
 ribbon = Frame(root,bg="yellow",relief=GROOVE,borderwidth = 6,width = 1080,height=50)
+
+#mainbox = ComboB(ribbon)
+featureCombo = ComboB(ribbon)
+leftBut = Button(ribbon, text = "<", command = leftCallback)
+leftBut.pack(padx=(200,0),side=LEFT)
+rightBut = Button(ribbon, text = ">", command = rightCallback)
+rightBut.pack(side=LEFT)
+
+
+boldBut = Button(ribbon, text = "B", command = boldCallback)
+boldBut.pack(padx=(100,0),side=LEFT)
+italicBut = Button(ribbon, text = "I", command = italicCallback)
+italicBut.pack(side=LEFT)
+underlineBut = Button(ribbon, text = "U", underline = 0, command = underlineCallback)
+underlineBut.pack(side=LEFT)
+
 ribbon.pack(fill="x",anchor ="nw")
 
-#ComboBox (MAIN) -- Create a combobox that updates it's contents based on state. 
-    
-class ComboB:
-    def __init___(self):
-        self.box = Combobox(ribbon,values=form_list("NONE"),postcommand = self.update_list())
-        self.box.pack()
-    def update_list(self):
-        self.box['values'] = form_list(self.box.get())
- 
-mainbox = ComboB()
 
 # Toolbar 
 toolbar = Frame(root,bg="grey",relief=GROOVE,borderwidth = 6,width = 250, height = 335)
--- logic.py
+++ logic.py
@@ -1,12 +1,21 @@
 STATE = "NONE" #use for defining state 
 
 def form_list(STATE):
+    set_list = []
     if STATE == "LEVEL":
         set_list = ["LEVEL","SUB-LEVEL","SCENE"]
     elif STATE == "SUB-LEVEL":
         set_list = ["LEVEL","SUB-LEVEL","SCENE"]
     elif STATE == "SCENE":
         set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
+    elif STATE == "OBJECT":
+        set_list = ["LEVEL"]
+    elif STATE == "OBJECTIVE":
+        set_list = ["LEVEL"]
+    elif STATE == "EVENT":
+        set_list = ["LEVEL"]
+    elif STATE == "ANIMATION":
+        set_list = ["LEVEL"]
     elif STATE == "NONE":
         set_list = ["LEVEL"]
     return set_list
