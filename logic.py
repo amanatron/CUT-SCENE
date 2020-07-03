692f7d5a386b47a2ecf21e196005be440c91cfc0
amanatron <amanatron@users.noreply.github.com>
2020-07-04 02:06:11 +0530

    made changes to the items in the list

--- logic.py
+++ logic.py
@@ -9,15 +9,15 @@ def form_list(STATE):
     elif STATE == "SCENE":
         set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     elif STATE == "OBJECT":
-        set_list = ["LEVEL"]
+        set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     elif STATE == "OBJECTIVE":
-        set_list = ["LEVEL"]
+        set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     elif STATE == "EVENT":
-        set_list = ["LEVEL"]
+        set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     elif STATE == "ANIMATION":
-        set_list = ["LEVEL"]
+        set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     elif STATE == "NONE":
-        set_list = ["LEVEL"]
+        set_list = ["LEVEL","SUB-LEVEL","SCENE","OBJECT","OBJECTIVE","EVENT","ANIMATION"]
     return set_list
     
         
